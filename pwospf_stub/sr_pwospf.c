891dddab213f1aacd5832f6f3ad6e99f325bd7f5
Natnael Daba <ndaba@arizona.edu>
2024-11-26 16:28:45 -0700

    Implement PWOSPF initialization and subsystem thread

--- pwospf_stub/sr_pwospf.c
+++ pwospf_stub/sr_pwospf.c
@@ -7,14 +7,16 @@
  *
  *---------------------------------------------------------------------------*/
 
-#include "sr_pwospf.h"
-#include "sr_router.h"
-
 #include <stdio.h>
 #include <unistd.h>
 #include <assert.h>
 #include <stdlib.h>
-
+#include <string.h>
+#include <arpa/inet.h>
+#include "sr_pwospf.h"
+#include "sr_if.h"
+#include "sr_router.h"
+#include "sr_router.h"
 
 /* -- declaration of main thread function for pwospf subsystem --- */
 static void* pwospf_run_thread(void* arg);
@@ -32,21 +34,54 @@ int pwospf_init(struct sr_instance* sr)
 {
     assert(sr);
 
-    sr->ospf_subsys = (struct pwospf_subsys*)malloc(sizeof(struct
-                                                      pwospf_subsys));
+    sr->ospf_subsys = (struct pwospf_subsys*)malloc(sizeof(struct pwospf_subsys));
 
     assert(sr->ospf_subsys);
     pthread_mutex_init(&(sr->ospf_subsys->lock), 0);
 
 
     /* -- handle subsystem initialization here! -- */
+    struct pwospf_subsys* subsys = sr->ospf_subsys;
 
+    /* -- set up router ID -- */
+    // Set Router ID to the IP of the first interface
+    struct sr_if* first_interface = sr->if_list;
+    if (first_interface) {
+        subsys->router_id = first_interface->ip;
+    } else {
+        fprintf(stderr, "Error: No interfaces available to determine Router ID\n");
+        subsys->router_id = 0; // Fallback value
+    }
+
+    // Set the area ID (single area for this project)
+    subsys->area_id = 0;
+
+    // Initialize the interface list in the PWOSPF subsystem
+    subsys->interfaces = NULL;
+    struct sr_if* iface = sr->if_list;
+    struct pwospf_interface** pwospf_iface_ptr = &(subsys->interfaces);
+    while (iface) {
+        // Allocate and populate PWOSPF interface structure
+        struct pwospf_interface* pwospf_iface = (struct pwospf_interface*)malloc(sizeof(struct pwospf_interface));
+        pwospf_iface->ip = iface->ip;
+        pwospf_iface->mask = iface->mask;
+        pwospf_iface->helloint = HELLO_INTERVAL;
+        pwospf_iface->neighbors = NULL;
+        pwospf_iface->next = NULL;
+
+        *pwospf_iface_ptr = pwospf_iface; // Append to the list
+        pwospf_iface_ptr = &(pwospf_iface->next); // Move to the next pointer
+        iface = iface->next; // Move to the next interface
+    }
 
     /* -- start thread subsystem -- */
-    if( pthread_create(&sr->ospf_subsys->thread, 0, pwospf_run_thread, sr)) {
+    if( pthread_create(&sr->ospf_subsys->thread, 0, pwospf_run_thread, sr) != 0) {
         perror("pthread_create");
         assert(0);
     }
+
+    printf("PWOSPF subsystem initialized with Router ID: %s\n",
+           inet_ntoa(*(struct in_addr*)&subsys->router_id));   
 
     return 0; /* success */
 } /* -- pwospf_init -- */
@@ -85,8 +120,7 @@ void pwospf_unlock(struct pwospf_subsys* subsys)
  *
  *---------------------------------------------------------------------*/
 
-static
-void* pwospf_run_thread(void* arg)
+static void* pwospf_run_thread(void* arg)
 {
     struct sr_instance* sr = (struct sr_instance*)arg;
 
@@ -95,6 +129,7 @@ void* pwospf_run_thread(void* arg)
         /* -- PWOSPF subsystem functionality should start  here! -- */
 
         pwospf_lock(sr->ospf_subsys);
+        pwospf_print_subsys(sr->ospf_subsys);
         printf(" pwospf subsystem sleeping \n");
         pwospf_unlock(sr->ospf_subsys);
         sleep(2);
@@ -102,4 +137,22 @@ void* pwospf_run_thread(void* arg)
     };
     return NULL;
 } /* -- run_ospf_thread -- */
+
+void pwospf_print_subsys(struct pwospf_subsys* subsys) {
+    assert(subsys);
+
+    printf("\nPWOSPF Subsystem State:\n");
+    printf("Router ID: %s\n", inet_ntoa(*(struct in_addr*)&subsys->router_id));
+    printf("Area ID: %d\n", subsys->area_id);
+
+    printf("Interfaces:\n");
+    struct pwospf_interface* iface = subsys->interfaces;
+    while (iface) {
+        printf("  - IP: %s\n", inet_ntoa(*(struct in_addr*)&iface->ip));
+        printf("    Mask: %s\n", inet_ntoa(*(struct in_addr*)&iface->mask));
+        printf("    HELLO Interval: %d\n", iface->helloint);
+        iface = iface->next;
+    }
+    printf("\n");
+}
 
--- pwospf_stub/sr_pwospf.h
+++ pwospf_stub/sr_pwospf.h
@@ -11,21 +11,50 @@
 #define SR_PWOSPF_H
 
 #include <pthread.h>
+#include <stdint.h>
+#include <time.h>
 
 /* forward declare */
 struct sr_instance;
 
+/* pwospf constants */
+#define PWOSPF_VERSION 2
+#define PWOSPF_TYPE_HELLO 1
+#define PWOSPF_TYPE_LSU 4
+#define HELLO_INTERVAL 10
+#define NEIGHBOR_TIMEOUT (3 * HELLO_INTERVAL)
+
+/* pwospf neighbor structure */
+struct pwospf_neighbor {
+    uint32_t router_id;
+    uint32_t neighbor_ip;
+    time_t last_hello_received;
+    struct pwospf_neighbor *next; // Linked list for neighbors
+};
+
+/* pwospf interface structure */
+struct pwospf_interface {
+    uint32_t ip;                      // IP address of the interface
+    uint32_t mask;                    // Subnet mask
+    uint16_t helloint;                // HELLO interval (default: 10 seconds)
+    struct pwospf_neighbor* neighbors; // List of neighbors reachable via this interface
+    struct pwospf_interface* next;    // Pointer to the next interface (linked list)
+};
+
 struct pwospf_subsys
 {
     /* -- pwospf subsystem state variables here -- */
-
+    uint32_t router_id; // ID of the router, typically the IP of the first interface
+    uint32_t area_id; // Single OSPF area for this project (set to 0)
+    struct pwospf_interface *interfaces; // Linked list of router interfaces
 
     /* -- thread and single lock for pwospf subsystem -- */
-    pthread_t thread;
-    pthread_mutex_t lock;
+    pthread_t thread; // HELLO thread
+    pthread_mutex_t lock; // Mutex lock for thread synchronization
 };
 
 int pwospf_init(struct sr_instance* sr);
-
+void* pwospf_hello_thread(void* arg);
+void pwospf_print_subsys(struct pwospf_subsys* subsys);
 
 #endif /* SR_PWOSPF_H */
